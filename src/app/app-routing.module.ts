cfa0f07021be511d23f74603f21e2943f074252b
Jefino9488 <jefinojacob9488@gmail.com>
2024-05-06 22:54:02 +0530

    reformat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from './home/home.component';
 import {ProjectsComponent} from './projects/projects.component';
 
 const routes: Routes = [
-    {path: '', component: HomeComponent, pathMatch: 'full'},
+    {path: '', redirectTo: "/home", pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'projects', component: ProjectsComponent},
