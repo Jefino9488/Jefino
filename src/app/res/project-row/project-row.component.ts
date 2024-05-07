92e0810c5b24d2920e0cdcecbc0ae06aa7a6a9b2
Jefino9488 <jefinojacob9488@gmail.com>
2024-05-07 22:38:50 +0530

    project-rows

---
+++ src/app/res/project-row/project-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-row',
+  templateUrl: './project-row.component.html',
+  styleUrl: './project-row.component.css'
+})
+export class ProjectRowComponent {
+
+}
